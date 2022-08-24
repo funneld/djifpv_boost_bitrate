e6d47ba15802708e34f96126367507d226517dfa
funneld <funneld@gmail.com>
2022-08-24 20:35:07 +0200

    send cmd for 15s

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 # DJI FPV Bitrate booster
 
-Increases the bitrate to 30Mbit in 25Mbit mode and 59.9Mbit in 50Mbit mode.
+Increases the bitrate up to 30Mbit in 25Mbit mode and 59.9Mbit in 50Mbit mode.
 
 
-## Installation
+## Reported side effects
+- higher latency(delay).
+- more missed frames that cause a stuttering effect. This is more noticeable in fast flights.
+- seemingly more issues with Vistas than standard Airunits. Possibly due to some extra heat generated when encoding at a higher bitrate(unconfirmed).
+
+## Manual installation
 
 Install:
 - `download ipk`
 - `opkg install package-name.ipk`
 
-
-Or manually:
+Compile:
 - `make ipk`
-- adb push ...
-- `opkg install "package-name"`
-
--- jni/boost_bitrate.c
+++ jni/boost_bitrate.c
@@ -6,20 +6,18 @@
 #include <string.h>
 #include "boost_bitrate.h"
 
-int32_t _ZN19GlassRacingChnlMenu7timeOutEv (void* this) {
-
-	int32_t ret_ = 0;
+void _ZN19GlassRacingChnlMenu7timeOutEv(void* this) {
 
 	if (!timeOut && !getInstance){
 
 		timeOut = dlsym (RTLD_NEXT, "_ZN19GlassRacingChnlMenu7timeOutEv");
 		if (timeOut == 0){
-			guiLib = dlopen("/system/lib/libtp1801_gui.so", 1);
+			if(!guiLib){guiLib = dlopen("/system/lib/libtp1801_gui.so", 1);}
 			timeOut = dlsym (guiLib, "_ZN19GlassRacingChnlMenu7timeOutEv");
 			if (timeOut == 0)
 			{
 				printf("dlsym: %s\n", dlerror());
-				return 0;
+				return;
 			}
 		}
 
@@ -30,43 +28,38 @@ int32_t _ZN19GlassRacingChnlMenu7timeOutEv (void* this) {
 			if (getInstance == 0)
 			{
 				printf("dlsym: %s\n", dlerror());
-				return 0;
+				return;
 			}
 		}
 	}
 
-	uint32_t inst = getInstance();
+	if(!instance)instance = getInstance();
 
-	uint32_t *gs_gui_config = (uint32_t *)*(uint32_t *)((int)inst + 0xe4);
-	uint32_t *gs_info = (uint32_t *)*(uint32_t *)((int)gs_gui_config + 0x4c);
+	if(instance){
+		gs_gui_config = (__gs_gui_config *)*(uint32_t *)((int)instance + 0xe4);
+	
+		gs_link_stat_t link = GS_LINK_STAT_UKNOWN;
+		gs_gui_config->gs_modem_get_link_state_wrap(gs_gui_config->gs_info, &link);
+		
+		if(link != GS_LINK_STAT_NORMAL){
+			clock_gettime(CLOCK_MONOTONIC, &start);
+		}
 
-	gs_link_stat_t link = GS_LINK_STAT_UKNOWN;
-	gs_link_stat_t *link_state = &link;
+		clock_gettime(CLOCK_MONOTONIC, &now);
+		
+		//GlassRacingChnlMenu::timeOut timeouts 1/s. Send the cmd every second for 15 seconds.	
+		if((link == GS_LINK_STAT_NORMAL) && !started){
+			if((now.tv_sec - start.tv_sec) > 15)started = true;
+			gs_gui_config->gs_modem_set_bandwidth_mode(gs_gui_config->gs_info, 6);
+			printf("Launching 30/60Mbit mode...\n");
+		}
+		else if((link == GS_LINK_STAT_LOST) && started){
+			started = false;
+			printf("LINK LOST!\n");
+		}
 
-	gs_modem_get_link_state_wrap = (void *)*(int32_t *)((int)gs_gui_config + 0x228);
-	gs_modem_set_bandwidth_mode = (void *)*(uint32_t *)((int)gs_gui_config + 0x2AC);
-
-	gs_modem_get_link_state_wrap(gs_info, link_state);
-
-
-	if(link != GS_LINK_STAT_NORMAL){
-		clock_gettime(CLOCK_MONOTONIC, &start);
 	}
-
-	clock_gettime(CLOCK_MONOTONIC, &now);
-
-	if((link == GS_LINK_STAT_NORMAL) && !started && ((now.tv_sec - start.tv_sec) > 9)){
-		started = true;
-		gs_modem_set_bandwidth_mode(gs_info, 6);
-		printf("Launch 30/60Mbit mode...\n");
-	}
-	else if((link == GS_LINK_STAT_LOST) && started){
-		started = false;
-		printf("LINK LOST!\n");
-	}
-
-
-	ret_ = timeOut(this);
-
-	return ret_;
+	
+	timeOut(this);
+	return;
 }
